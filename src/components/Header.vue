de05ae508fafa4abb1d6ddf35ada938b1d78f54c
konglao <827600127@qq.com>
2019-08-29 15:42:37 +0800

    pc端顶部导航栏初步完成
    强行写了两个鼠标监听事件

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,8 +8,34 @@
       </div>
       <div class="right">
         <ul>
-          <li>dance works</li>
-          <li>dance storys</li>
+          <li v-on:mouseenter="visible1" @mouseleave="display1">
+            <router-link to="/">dance storys</router-link>
+            <ul v-show="seen1" class="display">
+              <li>
+                <router-link to="/">wind</router-link>
+              </li>
+              <li>
+                <router-link to="/">big fish</router-link>
+              </li>
+              <li>
+                <router-link to="/about">say something</router-link>
+              </li>
+            </ul>
+          </li>
+          <li v-on:mouseenter="visible2" @mouseleave="display2">
+            <router-link to="/">dance works</router-link>
+            <ul v-show="seen2" class="display">
+              <li>
+                <router-link to="/">wind</router-link>
+              </li>
+              <li>
+                <router-link to="/">big fish</router-link>
+              </li>
+              <li>
+                <router-link to="/">say something</router-link>
+              </li>
+            </ul>
+          </li>
           <li>
             <router-link to="/about">about me</router-link>
           </li>
@@ -21,7 +47,27 @@
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
+  data: function() {
+    return {
+      seen1: false,
+      seen2: false
+    };
+  },
+  methods: {
+    visible1: function() {
+      this.seen1 = true;
+    },
+    display1: function() {
+      this.seen1 = false;
+    },
+    visible2: function() {
+      this.seen2 = true;
+    },
+    display2: function() {
+      this.seen2 = false;
+    }
+  }
 };
 </script>
 
@@ -38,12 +84,23 @@ export default {
 }
 .right {
   @include heading-2;
-  ul {
+  > ul {
     list-style: none;
-    li {
+    > li {
       display: inline-block;
       margin: 58px 26px;
+      > .display {
+        list-style: none;
+        position: absolute;
+        > li {
+          margin: 29px 0;
+        }
+      }
     }
   }
+}
+a {
+  text-decoration: none;
+  color: $black;
 }
 </style>
